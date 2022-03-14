81a5edc37677c6ffa5476b115cad141a2d8b654d
Cleber <kleberdejesus45@hotmail.com>
2022-03-14 13:39:59 -0300

    chore: reestruturação do html

--- Xadrez/main.js
+++ Xadrez/main.js
@@ -3,19 +3,28 @@ let isPartIdToken = false
 let partplay
 
 function playTime(part) {
-  console.log(part)
+  let classValue = []
+  part.classList.forEach(partClass => {
+    classValue.push(partClass)
+  })
+  if (
+    classValue.find(clas => clas === 'pawn') === 'pawn' &&
+    classValue.find(clas => clas === 'white') === 'white'
+  ) {
+    part.addEventListener('click', () => {
+      part.id = ''
+      isPartIdToken = false
+      return isPartIdToken
+    })
+  }
 }
 
 game.forEach(id => {
   let whitePart = id.querySelectorAll('.white')
   partplay = whitePart.forEach(part => {
-    part.classList.forEach(partClass => {
-      if (partClass === 'token') {
-      }
-    })
     part.addEventListener('click', () => {
       if (isPartIdToken !== true) {
-        part.id = 'token'
+        part.id = 'focus'
         isPartIdToken = true
         playTime(part)
       }
