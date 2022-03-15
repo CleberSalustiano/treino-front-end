df4c8a1d5bf0b97d976b6d149358a639ced04c7e
Cleber <kleberdejesus45@hotmail.com>
2022-03-15 13:50:11 -0300

    chore: tentativas, possibilidade de uma solução

--- Xadrez/main.js
+++ Xadrez/main.js
@@ -1,36 +1,81 @@
-let game = document.querySelectorAll('ul[id]')
-let isPartIdToken = false
-let partplay
+/* 
+  Refazer completamente o js
+*/
 
-function playTime(part) {
-  let classValue = []
-  part.classList.forEach(partClass => {
-    classValue.push(partClass)
+/*
+let html = ''
+game.forEach(line => {
+  html = ''
+  let id = line.id
+  for (const element of positions[id]) {
+    html += element.outerHTML
+  }
+  console.log(html)
+  game[line] = html
+})
+
+game[0].innerHTML =
+  '<li><div class="tower white">Torre</div></li><li><div class="horse white">Cavalo</div></li><li><div class="bishop white">Bispo</div></li><li><div class="queen white">Rainha</div></li><li><div class="king white">Rei</div></li><li><div class="bishop white">Bispo</div></li><li><div class="horse white">Cavalo</div></li><li><div class="tower white">Torre</div></li>'
+*/
+
+let main = document.querySelector('#game')
+let game = document.querySelectorAll('ul[id]')
+let positions = {}
+let nodesParts = []
+// let blackParts = []
+// let whiteParts = []
+let isPartIdToken = false
+
+function get() {
+  game.forEach(line => {
+    let id = line.id
+    let nodes = line.children
+    positions[id] = nodes
+    if (line.querySelectorAll('div').length != 0) {
+      nodesParts.push(line.querySelectorAll('div'))
+    }
   })
+}
+
+function movimentPart(part, parts) {
   if (
-    classValue.find(clas => clas === 'pawn') === 'pawn' &&
-    classValue.find(clas => clas === 'white') === 'white'
+    part.classList.value.split(' ')[0] == 'pawn' &&
+    part.classList.value.split(' ')[1] == 'white'
   ) {
-    part.addEventListener('click', () => {
-      part.id = ''
-      isPartIdToken = false
-      return isPartIdToken
+    let isFind = []
+    parts.forEach(partFind => {
+      if (partFind.classList.value === part.classList.value) {
+        isFind.push(true)
+      } else {
+        isFind.push(false)
+      }
     })
+
+    console.log(isFind)
   }
 }
 
-game.forEach(id => {
-  let whitePart = id.querySelectorAll('.white')
-  partplay = whitePart.forEach(part => {
-    part.addEventListener('click', () => {
-      if (isPartIdToken !== true) {
-        part.id = 'focus'
-        isPartIdToken = true
-        playTime(part)
-      }
+/* Recriar para mantar para movimentPart a posição e a peça que vai ser movida */
+function lister() {
+  nodesParts.forEach(parts => {
+    parts.forEach(part => {
+      part.addEventListener('click', () => {
+        if (isPartIdToken !== true) {
+          part.classList.add('focus')
+          isPartIdToken = true
+          movimentPart(part, parts)
+        } else if (part.classList.value.split(' ')[2] == 'focus') {
+          part.classList.remove('focus')
+          isPartIdToken = false
+        }
+      })
     })
-    if (partplay != undefined) {
-      return partplay
-    }
   })
-})
+}
+
+get()
+lister()
+// pawn = blackParts.querySelector('pawn')
+// pawn.addEventListener('click', () => {
+//   console.log('here')
+// })
